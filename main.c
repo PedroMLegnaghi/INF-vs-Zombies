8289658526034c334d1ccec67f4046597e77d41b
Pedro Muller Legnaghi <157427799+PedroMLegnaghi@users.noreply.github.com>
2025-05-17 22:40:40 -0300

    adding exit confirmation and the screen that asks for the users's name

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include <stdbool.h>
 //refers to the screens of the game
     //LOGO(made by pedro)
-    //HOMEPAGE(START,LEADERBOARD,CONFIGURATIONS,ABOUT, EXIT)
-    //CONFIGURATIONS
-    //LEADERBOARD
-    //EXIT(isnt necessarily an screen, but let's consider it for the moment)
-    //START(asking name and gender)
-        //->GAMEOPTIONS (deck of plants)?
-            //GAMEPLAY
-typedef enum GameScreen {LOGO = 0, HOMEPAGE, GAMEPLAY, LEADERBOARD, ABOUT, CONFIGURATIONS, EXIT } GameScreen;
+    //HOMEPAGE(PLAY,LEADERBOARD,CONFIGURATIONS,ABOUT, EXIT)
+        //PLAY
+            //(asking name and gender)
+            //->GAMEOPTIONS (deck of plants)?
+        //CONFIGURATIONS
+        //LEADERBOARD
+        //EXIT(isnt necessarily an screen, but let's consider it for the moment)
+typedef enum GameScreen {LOGO = 0, HOMEPAGE, PLAY, LEADERBOARD, ABOUT, CONFIGURATIONS, EXIT //HOMEPAGE OPTIONS
+                         ,USER_DATA, DECK_SELECTION, GAMEPLAY} GameScreen;//GAMEPLAY OPTIONS
 #define HOME_PAGE_OPTIONS_QUANTITY 5 //quantity of options in the Homepage
 int main (void){
     //initialization
@@ -23,23 +24,23 @@ int main (void){
 //--------
 
 //--HomePage
-Rectangle homePageOptionsRec[HOME_PAGE_OPTIONS_QUANTITY]={0};//initializing array of rectangles that refer to the options of the game in the landpage(gameplay, leaderboard, about, configurations and exit)
+Rectangle homePageOptionsRec[HOME_PAGE_OPTIONS_QUANTITY]={0};//initializing array of rectangles that refer to the options of the game in the landpage(PLAY, leaderboard, about, configurations and exit)
 bool homePageOptionsRecHover[HOME_PAGE_OPTIONS_QUANTITY]={0};//array that tells if an option is hovered 
 GameScreen homePageOptions[HOME_PAGE_OPTIONS_QUANTITY] ={0};//array to navigate over the options of the game
-//Filling the homePageOptions
-homePageOptions[0]= GAMEPLAY;
-homePageOptions[1]= LEADERBOARD;
-homePageOptions[2]= ABOUT;
-homePageOptions[3]= CONFIGURATIONS;
-homePageOptions[4]= EXIT;
+    //Filling the homePageOptions
+    homePageOptions[0]= PLAY;
+    homePageOptions[1]= LEADERBOARD;
+    homePageOptions[2]= ABOUT;
+    homePageOptions[3]= CONFIGURATIONS;
+    homePageOptions[4]= EXIT;
 
-//Filling the homePageOptionsRec 
-for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
-    homePageOptionsRec[i].height = 40;
-    homePageOptionsRec[i].width = 240;
-    homePageOptionsRec[i].x=(screenWidth-MeasureText("INF vs ZOMBIES", 20))/2-30;
-    homePageOptionsRec[i].y = 110+60*i;
-}
+    //Filling the homePageOptionsRec 
+    for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
+        homePageOptionsRec[i].height = 40;
+        homePageOptionsRec[i].width = 240;
+        homePageOptionsRec[i].x=(screenWidth-MeasureText("INF vs ZOMBIES", 20))/2-30;
+        homePageOptionsRec[i].y = 110+60*i;
+    }
 //---------
 //--exiting application
     SetExitKey(KEY_NULL); // Disable KEY_ESCAPE to close window, X-button still works
@@ -48,6 +49,17 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
     int framesCounter = 0;          // Useful to count frames
     Vector2 mousePoint = { 0.0f, 0.0f }; //useful to track the user's mouse
     SetTargetFPS(60);               // Set desired framerate (frames-per-second)
+
+//--PLAY variables
+//146=sizeof name of Dom Pedro I
+const unsigned int MAX_SIZE_OF_NAME = 14+1;
+char playerName[MAX_SIZE_OF_NAME];
+    for (int i=0;i<MAX_SIZE_OF_NAME;i++)
+        playerName[i]='\0';
+    // playerName[0]='\0';
+    unsigned int sizeOfName = 0;//variable used to track the size of the name of the user
+
+//---------
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -56,12 +68,18 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
         // Update
         //----------------------------------------------------------------------------------
         mousePoint = GetMousePosition();//tracking, effectively, the user's mouse
+        
+        //if user wants to end applications, redirect him to an exit confirmation
+        if(WindowShouldClose()){
+            currentScreen=  EXIT;
+        }
+
         switch (currentScreen)
         {
             case LOGO:
             {
                 // TODO: Update LOGO screen variables here!
-
+                
                 framesCounter++;    // Count frames
 
                 // Wait for 7 seconds (420 frames) before jumping to HOMEPAGE screen
@@ -87,16 +105,53 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
         }
 
             } break;
-            case GAMEPLAY:
+            case PLAY:
             {
-                // TODO: Update GAMEPLAY screen variables here!
+                // TODO: Update PLAY screen variables here!
+                currentScreen = USER_DATA;//first thing to be done, is to ask for the User data
+                
+                
+               
+            } break;
+              case USER_DATA:
+            {
+                // TODO: Update USERDATA screen variables here!-------------------
+                previousScreen=currentScreen;
+                // Get char pressed (unicode character) on the queue
+                int key = GetCharPressed();
+                
+                // Check if more characters have been pressed on the same frame
+                while (key > 0) {
+                    // NOTE: Only allow keys in range [32..125]
+                    //MAX_SIZE_OF_NAME-1 to garantee the space for '\0'
+                    if ((key >= 32) && (key <= 125) && (sizeOfName < MAX_SIZE_OF_NAME-1)) {
+                        playerName[sizeOfName] = (char)key;
+                        sizeOfName++;
+                    }
 
-                // Press enter to change to ENDING screen
+                    key = GetCharPressed();  // Check next character in the queue
+                }
+
+                if (IsKeyPressed(KEY_BACKSPACE)) {
+                    if (sizeOfName == 0){
+                        sizeOfName = 0;
+                    }else{
+                        sizeOfName--;
+                    }
+                    playerName[sizeOfName] = '\0';
+                }
+
+                if (IsKeyPressed(KEY_ENTER) && sizeOfName > 0) {
+                    currentScreen = DECK_SELECTION;  // Ou qualquer próxima tela desejada
+                }
+
+
                
             } break;
             case ABOUT:
             {
                 // TODO: Update ENDING screen variables here!
+                
 
                 // Press enter to return to HOMEPAGE screen
                
@@ -134,16 +189,19 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
             {
                 case LOGO:
                 {
+                    ClearBackground(RAYWHITE);
                     // TODO: Draw LOGO screen here!
                     DrawText("Pedrokas's gaming enterprise® \n\t\t\t\t\t\t\t\tpresents... ", 70, 150, 40, DARKGREEN);
 
                 } break;
                 case HOMEPAGE:
                 {
+                    ClearBackground(RAYWHITE);
                     // TODO: Draw HOMEPAGE screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
                     for(int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
                         DrawRectangleRec(homePageOptionsRec[i], WHITE);
+                         DrawRectangleLines(homePageOptionsRec[i].x, homePageOptionsRec[i].y, homePageOptionsRec[i].width, homePageOptionsRec[i].height, BLACK);
                         //tracking hover over the options
                         if(homePageOptionsRecHover[i]==true){
                             DrawRectangleRec(homePageOptionsRec[i], RED);
@@ -156,16 +214,44 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
                     DrawText("CONFIGURATIONS", (screenWidth-MeasureText("INF vs ZOMBIES", 20))/2, 300, 20, DARKGREEN);
                     DrawText("EXIT", (screenWidth-MeasureText("INF vs ZOMBIES", 20))/2, 360, 20, DARKGREEN);
                 } break;
-                case GAMEPLAY:
+                case USER_DATA:
                 {
-                    // TODO: Draw GAMEPLAY screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                    DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-                    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+                    // TODO: Draw USERDATA screen here!
+                  
+                    ClearBackground(RAYWHITE);
+                    Rectangle textBox = { screenWidth/2 - 150, 180,  MeasureText("A",40)*8+30, 50 };
+                    if(sizeOfName>8){
+                        textBox.width = sizeOfName*MeasureText("A",40)+10;
+                    }
+                    
+                    DrawText("Tell me your name!", 240, 140, 20, GRAY);
+
+                    DrawRectangleRec(textBox, LIGHTGRAY);
+                    DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, RED);
+
+                    DrawText(playerName, textBox.x + 5, textBox.y + 8, 40, MAROON);
+
+                    DrawText(TextFormat("Press enter to confirm: %i/%i", sizeOfName, MAX_SIZE_OF_NAME-1), 315, 250, 20, DARKGRAY);
+
+                    if (sizeOfName < MAX_SIZE_OF_NAME) {
+                        // Draw blinking underscore char
+                     DrawText("_",( textBox.x + 8 + MeasureText(playerName, 40)),(textBox.y + 12), 40, MAROON);
+                    } 
+                  
+
+                } break;
+                 case DECK_SELECTION:
+                {
+                    ClearBackground(RAYWHITE);
+                    // TODO: Draw ENDING screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
+                    DrawText(TextFormat("Nice to meet you, %s :)", playerName), 20, 20, 40, DARKBLUE);
+                    DrawText("Select you deck", 120, 220, 20, DARKBLUE);
 
                 } break;
                 case ABOUT:
                 {
+                    ClearBackground(RAYWHITE);
                     // TODO: Draw ENDING screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
@@ -174,6 +260,7 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
                 } break;
                  case CONFIGURATIONS:
                 {
+                    ClearBackground(RAYWHITE);
                     // TODO: Draw ENDING screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
@@ -182,6 +269,7 @@ for (int i=0;i<HOME_PAGE_OPTIONS_QUANTITY;i++){
                 } break;
                  case EXIT:
                 {
+                    ClearBackground(RAYWHITE);
                     // TODO: Draw EXIT screen here!
                 DrawRectangle(0, 100, screenWidth, 200, BLACK);
                 DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
